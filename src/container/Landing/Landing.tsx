3ebd989f95459d25d4390301ff0c38087fb2cdb0
Daniel Fernandez <dfernandezcode@gmail.com>
2023-07-28 21:39:26 +0200

    updated home layout

--- src/container/Landing/Landing.tsx
+++ src/container/Landing/Landing.tsx
@@ -1,24 +1,21 @@
 import "./Landing.scss";
+import social_Github from "../../assets/images/social-icons/social_github.svg";
+import social_LinkedIn from "../../assets/images/social-icons/social_linkedin.svg";
 
 const Landing = () => {
   return (
     <div className="landing">
-      <div className="landing__menu">
-        <a href="#about" className="landing__menu--items">
-          About
-        </a>
-        <a href="#stack" className="landing__menu--items">
-          Skills
-        </a>
-        <a href="#projects" className="landing__menu--items">
-          Projects
-        </a>
-        <a href="#experience" className="landing__menu--items">
-          Work
-        </a>
-        <a href="#resume" className="landing__menu--items">
-          Resume
-        </a>
+      <div className="landing__wrapper">
+        <h1>Daniel Fernandez</h1>
+        <h2>Web designer & developer</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia.
+        </p>
+        <div className="landing__social">
+          <img src={social_Github} alt="Github" />
+          <img src={social_LinkedIn} alt="LinkedIn" />
+        </div>
       </div>
     </div>
   );
