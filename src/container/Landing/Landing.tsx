e18171b4044b65a86688af8deb0e8f57a208bd56
Daniel Fernandez <dfernandezcode@gmail.com>
2023-07-29 03:02:17 +0200

    homepage final design base complete. TODO: add radial mouseover effect, add hover effects. Finalize colours and links

--- src/container/Landing/Landing.tsx
+++ src/container/Landing/Landing.tsx
@@ -6,15 +6,35 @@ const Landing = () => {
   return (
     <div className="landing">
       <div className="landing__wrapper">
-        <h1>Daniel Fernandez</h1>
-        <h2>Web designer & developer</h2>
-        <p>
+        <h1 className="landing__name">Daniel Fernandez</h1>
+        <h2 className="landing__title">Web designer & developer</h2>
+        <p className="landing__blurb">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
           mollitia.
         </p>
         <div className="landing__social">
-          <img src={social_Github} alt="Github" />
-          <img src={social_LinkedIn} alt="LinkedIn" />
+          <a
+            href="https://github.com/Dfernandezcode"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="landing__social-icon"
+              src={social_Github}
+              alt="Github"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/dfernandezfoe/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="landing__social-icon"
+              src={social_LinkedIn}
+              alt="LinkedIn"
+            />
+          </a>
         </div>
       </div>
     </div>
