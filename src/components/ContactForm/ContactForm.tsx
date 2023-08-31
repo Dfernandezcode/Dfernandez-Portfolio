a4d7796266a38f3b82c021fd19f7159c19bb7be0
Daniel Fernandez <dfernandezcode@gmail.com>
2023-08-31 21:58:01 +0200

    email error messaging added, form validation added, education history completed

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,13 +1,33 @@
+import { useState, FormEvent } from 'react';
 import { useForm, ValidationError } from "@formspree/react";
 import "./ContactForm.scss";
 
 function ContactForm() {
   const [state, handleSubmit] = useForm("xaygeoan");
+  const [email, setEmail] = useState(''); 
+  const [message, setMessage] = useState(''); 
+  const [emailError, setEmailError] = useState(false); 
+  const [messageError, setMessageError] = useState(false); 
+  const validate = () => {
+    setEmailError(!email);
+    setMessageError(message.length < 20);
+    return email && message.length >= 20;
+  };
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    if (validate()) {
+      handleSubmit(e);
+    } else {
+      e.preventDefault();
+    }
+  };
+
   if (state.succeeded) {
     return <p>Thanks for joining!</p>;
   }
+
   return (
-    <form onSubmit={handleSubmit} className="form-email">
+    <form onSubmit={handleFormSubmit} className="form-email">
       <label className="form-email__address" htmlFor="email">
         Email Address:
       </label>
@@ -16,17 +36,31 @@ function ContactForm() {
         id="email"
         type="email"
         name="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
       />
+      {emailError && <p className='form-email__input-error'>Please input an email.</p>}
       <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <textarea className="form-email__textarea" id="message" name="message" />
+      
+      <textarea
+        className="form-email__textarea"
+        id="message"
+        name="message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      {messageError && <p className='form-email__input-error'>Please input a message with at least 20 characters</p>}
       <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <button
-        className="form-email__submit"
-        type="submit"
-        disabled={state.submitting}
-      >
-        Submit
-      </button>
+      
+      <div className="form-email__btn-wrapper">
+        <button
+          className="form-email__submit"
+          type="submit"
+          disabled={state.submitting}
+        >
+          Submit
+        </button>
+      </div>
     </form>
   );
 }
