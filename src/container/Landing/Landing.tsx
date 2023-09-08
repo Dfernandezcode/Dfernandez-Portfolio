3b12fd761812a7bfba96ab1d8f2dc4e21cfcd36f
Daniel Fernandez <dfernandezcode@gmail.com>
2023-09-08 10:34:12 +0200

    case sensitivity error

--- src/container/Landing/Landing.tsx
+++ src/container/Landing/Landing.tsx
@@ -1,8 +1,8 @@
 import "./Landing.scss";
 import social_Github from "../../assets/images/social-icons/social_github.svg";
 import social_GithubLight from "../../assets/images/social-icons/social_github-black.svg";
-import social_LinkedIn from "../../assets/images/social-icons/social_linkedIn.svg";
-import social_LinkedInLight from "../../assets/images/social-icons/social_linkedIn-black.svg";
+import social_LinkedIn from "../../assets/images/social-icons/social_linkedin.svg";
+import social_LinkedInLight from "../../assets/images/social-icons/social_linkedin-black.svg";
 
 interface Props {
   theme: string;
