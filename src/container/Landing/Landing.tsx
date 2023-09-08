b0a1d334a298d8adaf18db7168e7ab4b26e7f16b
Daniel Fernandez <dfernandezcode@gmail.com>
2023-09-08 21:06:11 +0200

    adjusting theme selector position

--- src/container/Landing/Landing.tsx
+++ src/container/Landing/Landing.tsx
@@ -12,7 +12,7 @@ const Landing: React.FC<Props> = ({ theme }) => {
     <main className="landing">
       <div className="landing__wrapper">
         <a className="landing__link" href="index.html">
-          <h1 className="landing__name">Daniel Fernandez</h1>
+          <h1 className="landing__name">Daniel Fernández</h1>
         </a>
         <h2 className="landing__title">Designer & Developer</h2>
         <div className="landing__social">
