e925e0d87bb05d7e5c3e7e3bff5e8650d9a52395
Daniel Fernandez <dfernandezcode@gmail.com>
2023-09-10 03:02:31 +0200

    fixed link referral, submit button hover

--- src/container/Projects/ProjectList/Tmdb.tsx
+++ src/container/Projects/ProjectList/Tmdb.tsx
@@ -4,13 +4,13 @@ const Project_Tmdb = () => {
   return (
     <div className="projects__card">
       <h3 className="projects__card--title">
-        <a className="projects__card--link" href="https://thedbmovie.netlify.app/">
+        <a className="projects__card--link" href="https://thedbmovie.netlify.app/" target="_blank" rel="noopener noreferrer">
           Movie wiki ↗
         </a>
       </h3>
       <p className="projects__card--text">
         Developed using the{" "}
-        <a className="projects__card--link" href="https://developer.themoviedb.org/reference/search-movie">
+        <a className="projects__card--link" href="https://developer.themoviedb.org/reference/search-movie" target="_blank" rel="noopener noreferrer">
           TMDB API ↗
         </a>{" "}
         with React and supporting technologies.
