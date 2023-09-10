c9796d3d8c138c43d2db649c6cdacb020f4a6c52
Daniel Fernandez <dfernandezcode@gmail.com>
2023-09-10 02:15:09 +0200

    movie api project added

--- src/container/Projects/ProjectList/Tmdb.tsx
+++ src/container/Projects/ProjectList/Tmdb.tsx
@@ -4,16 +4,16 @@ const Project_Tmdb = () => {
   return (
     <div className="projects__card">
       <h3 className="projects__card--title">
-        Movie wiki ↗
+        <a className="projects__card--link" href="https://thedbmovie.netlify.app/">
+          Movie wiki ↗
+        </a>
       </h3>
       <p className="projects__card--text">
-       Developed using the{" "}
-        <a
-          className="projects__card--link"
-          href="https://developer.themoviedb.org/reference/search-movie"
-        >
+        Developed using the{" "}
+        <a className="projects__card--link" href="https://developer.themoviedb.org/reference/search-movie">
           TMDB API ↗
-        </a>{" "} with React and supporting technologies.
+        </a>{" "}
+        with React and supporting technologies.
       </p>
       <div className="techbox">
         <p className="techbox__item">React</p>
